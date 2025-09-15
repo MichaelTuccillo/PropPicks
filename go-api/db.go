8da0ce3d15c52f1a11e69b35c2d6e097222157eb
MichaelTuccillo <michaeltuccillo5@gmail.com>
2025-09-14 21:55:46 -0400

    "DB: force IPv4 dial for Render (tcp4) + fast ping"

--- go-api/db.go
+++ go-api/db.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
+	"net"
+	"os"
+	"time"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
-func openDB(dsn string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("open db: %v", err)
+func openDB() *sql.DB {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("[DB] missing DATABASE_URL")
 	}
+
+	// Parse DSN and force IPv4 to avoid IPv6-only routes on some hosts
+	cfg, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		log.Fatalf("[DB] parse DSN: %v", err)
+	}
+	cfg.DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		d := &net.Dialer{Timeout: 5 * time.Second, KeepAlive: 30 * time.Second}
+		// Force IPv4
+		return d.DialContext(ctx, "tcp4", addr)
+	}
+
+	db := stdlib.OpenDB(*cfg)
+
+	// Reasonable pool settings for Render free/starter dynos
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(30 * time.Minute)
+
+	// Fast fail if unreachable
+	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("[DB] connect failed: %v", err)
+	}
+
+	log.Println("[DB] connected")
 	return db
 }
+
 
 // AutoMigrate all app tables.
 // NOTE: PastBetRecord is defined in past_bets.go
--- go-api/go.mod
+++ go-api/go.mod
@@ -1,26 +1,26 @@
 module example.com/go-api
 
 go 1.25
+
 toolchain go1.25.1
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/go-chi/cors v1.2.2
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/jackc/pgx/v5 v5.6.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.31.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.3
 )
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/rs/cors v1.11.1 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
